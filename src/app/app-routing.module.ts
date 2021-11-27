747d5f681d740b3373d2c25d9286228821278584
AlexSilvaE <alex2001ene@gmail.com>
2021-11-27 00:39:36 -0500

    Estructura base del proyecto, apartado público y privado OK. No se ha trabajado los templates. Básico

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  // ruta comodín
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      scrollPositionRestoration: 'enabled'
+    }
+    )],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
